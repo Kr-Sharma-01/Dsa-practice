84246ae7f0d3a777522442fc8f0161fa831de233
Krish Sharma <krishsharma2425@gmail.com>
2026-06-25 23:58:59 +0530

    Updated question

--- array_rotate.cpp
+++ array_rotate.cpp
@@ -3,9 +3,10 @@
 
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
-
+ /*
 int rotate (vector<int> & arr){
     int temp = arr[0];
     for (int i =1 ; i<arr.size() ; i++){
@@ -14,11 +15,23 @@ int rotate (vector<int> & arr){
     arr[arr.size()-1] = temp;
     return 0;
 }    
+*/
+
+// Rotating array k times left 
+
+void rotate(vector<int> & arr , int k){
+    int n = arr.size();
+    k = k%n;
+    reverse(arr.begin() , arr.begin()+k);
+    reverse(arr.begin()+k , arr.end());
+    reverse(arr.begin() , arr.end());
+}
+
 
 int main (){
     vector<int> arr = {1,2,3,4,5,6};
-
-    rotate(arr);
+    int k = 3;
+    rotate(arr , k);
 
     cout << "Rotated array is : ";
     for (int x:arr){
