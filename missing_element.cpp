859931a044bcd6b12aa90446d806310454d5d0fa
Krish Sharma <krishsharma2425@gmail.com>
2026-07-03 00:21:06 +0530

    New Question

--- missing_element.cpp
+++ missing_element.cpp
@@ -29,7 +29,7 @@ int missing (vector<int> & arr){
 int missing (vector<int> & arr , int N){
     int xor1 = 0 ; 
     int xor2 = 0; 
-    int n = N-1;
+
     for(int i = 0 ; i < arr.size() ; i++){
         xor2 = xor2 ^ arr[i];
         xor1 = xor1 ^(i+1);
