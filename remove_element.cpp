6d27a7559ce58de0dab996efa1d68d08765f9acb
Krish Sharma <krishsharma2425@gmail.com>
2026-07-04 23:50:40 +0530

    Qus

---
+++ remove_element.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+
+    int main() {
+    vector<int> arr = {1,2,3,4,5};
+    int ans = removeElement(arr, 5);
+    cout << ans ; 
+    return 0;
+    }
