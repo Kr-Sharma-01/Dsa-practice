611029031bde1ab5c1a45305cb97515d8dd6a5f9
Krish Sharma <krishsharma2425@gmail.com>
2026-07-05 23:21:39 +0530

    qus

---
+++ sort_012.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> sorting (vector<int> & arr){
+    int low = 0 , mid = 0 , high = arr.size()-1 ;
+    while(mid <= high){
+
+        if(arr[mid]==0){
+            swap(arr[mid] , arr[low]);
+            low++;
+            mid++;
+        }
+
+        else if (arr[mid]==1){
+            mid++;
+        }
+
+        else {
+            swap(arr[mid] , arr[high]);
+            high--;
+        }
+    }
+return arr;
+}
+
+
+int main (){
+    vector<int> arr  = {0,1,0,1,1,0,2,1,0,2};
+    vector<int> ans = sorting(arr);
+    for(int x : ans){
+        cout << x << " ";
+    }
+    return 0;
+}
