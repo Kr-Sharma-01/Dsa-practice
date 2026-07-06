563252f3fac23e3dc469fd4d211b08ee396b0700
Krish Sharma <krishsharma2425@gmail.com>
2026-07-06 23:51:43 +0530

    Leetcode

--- majority1.cpp
+++ majority1.cpp
@@ -1,8 +1,13 @@
+// Majority element 1 (Leetcode)
+
 #include<iostream>
 #include<vector>
 
 using namespace std;
 
+// Brute soln : 
+
+/*
 int majorityElement (vector<int> & arr){
     for(int i =0 ; i< arr.size() ; i++){
         int count =0 ; 
@@ -17,9 +22,31 @@ int majorityElement (vector<int> & arr){
     }
  return 0;
 }
+*/
+
+// Optimal soln :
+
+int majorityElement (vector<int> & arr){
+    int candidate = 0;
+    int count = 0 ;
+    for(int i = 0 ; i < arr.size() ; i++){
+        if (count == 0){
+            candidate = arr[i];
+        }
+        if (arr[i] == candidate){
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+    return candidate;
+}
+
 
 int main (){
     vector<int> arr = {1,1,4,4,4,3,4};
     int ans = majorityElement(arr);
     cout << ans;
+    return 0;
 }
