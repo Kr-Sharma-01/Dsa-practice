e8395b108f8958b511547bdbbd1ecd67c3d346cb
Krish Sharma <krishsharma2425@gmail.com>
2026-07-12 23:59:01 +0530

    Leetcode

---
+++ stock1.cpp
@@ -0,0 +1,46 @@
+// Stock buy and sell max profit (Leetcode)
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+int maxProfit(vector<int>& prices) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < prices.size(); i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            int profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of days: ";
+    cin >> n;
+
+    vector<int> prices(n);
+
+    cout << "Enter stock prices: ";
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
+    int ans = maxProfit(prices);
+
+    cout << "Maximum Profit = " << ans << endl;
+
+    return 0;
+}
