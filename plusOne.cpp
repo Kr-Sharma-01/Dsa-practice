c8fe5f157a76c5b52e8a2eb69e2ffbe85274e63b
Krish Sharma <krishsharma2425@gmail.com>
2026-07-15 00:04:44 +0530

    Leetcode

--- plusOne.cpp
+++ plusOne.cpp
@@ -1,8 +1,29 @@
+// Plus one digit (Leetcode)
+
 #include<iostream>
 #include<vector>
 
 using namespace std;
 
-int addOne (vector<int> & arr){
-    for (int i < arr.size()-1 ; i>=0 ; i--)
+// Optimal Soln :
+
+vector<int> addOne (vector<int> & arr){
+    for (int i = arr.size()-1 ; i>=0 ; i--){
+        if (arr[i]<9){
+            arr[i]++;
+            return arr;
+        }
+        arr[i]=0;
+    }
+    arr.insert(arr.begin() ,1);    return arr;
+}
+
+int main (){
+    vector<int> arr = {1,2,3};
+    vector<int> ans = addOne(arr);
+    cout << "The plus one number is : ";
+    for ( int x  : ans){
+        cout << x ;
+    }
+    return 0;
 }
