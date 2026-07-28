7a9eb8503cb118c893b712c4c6bd0e5f0a2de54d
Krish Sharma <krishsharma2425@gmail.com>
2026-07-28 23:39:07 +0530

    Leetcode

--- maxSubarrayK.cpp
+++ maxSubarrayK.cpp
@@ -13,27 +13,21 @@ int maxSubArray(vector<int>& nums) {
         int maximum = nums[0];
 
         for (int i = 0; i < nums.size(); i++) {
-
             sum += nums[i];
-
             if (sum > maximum) {
                 maximum = sum;
             }
-
             if (sum < 0) {
                 sum = 0;
             }
         }
-
         return maximum;
     }
 
 
 int main() {
 
-       vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     cout << "Maximum Subarray Sum = " << maxSubArray(nums);
-
     return 0;
 }
