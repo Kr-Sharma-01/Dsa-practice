85e861747c33a86b2bbfd8a2a8f723912a34a36e
Krish Sharma <krishsharma2425@gmail.com>
2026-07-30 23:44:32 +0530

    Leetcode

--- maxSubarrayK.cpp
+++ maxSubarrayK.cpp
@@ -8,10 +8,8 @@ using namespace std;
 // Optimal soln :
 
 int maxSubArray(vector<int>& nums) {
-
         int sum = 0;
         int maximum = nums[0];
-
         for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
             if (sum > maximum) {
@@ -22,11 +20,10 @@ int maxSubArray(vector<int>& nums) {
             }
         }
         return maximum;
-    }
+   }
 
 
 int main() {
-
     vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
     cout << "Maximum Subarray Sum = " << maxSubArray(nums);
     return 0;
