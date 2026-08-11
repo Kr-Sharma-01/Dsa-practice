d3b121e0ca17d472f1cdef4e1f0112822db0d980
Krish Sharma <krishsharma2425@gmail.com>
2026-08-11 23:33:13 +0530

    Leetcode

--- smallest _integer.cpp
+++ smallest _integer.cpp
@@ -1,29 +1,41 @@
-#include<iostream>
-#include<vector>
-#include<unordered_set>
+// Smallest Missing Integer Greater Than Sequential Prefix Sum (leetcode) 
 
+#include <iostream>
+#include <vector>
+#include <unordered_set>
 using namespace std;
 
-class Solution {
-public:
-    int missingInteger(vector<int>& nums) {
-        int sum = nums[0];
+// Optimal soln :
 
-        // Find sequential prefix sum
-        int i = 1;
-        while (i < nums.size() && nums[i] == nums[i - 1] + 1) {
-            sum += nums[i];
-            i++;
-        }
+int missingInteger(vector<int>& nums) {
+    int sum = nums[0];
 
-        // Store all numbers in a set
-        unordered_set<int> st(nums.begin(), nums.end());
+    // Find sum of the sequential prefix
+    int i = 1;
 
-        // Find smallest integer >= sum that is missing
-        while (st.count(sum)) {
-            sum++;
-        }
-
-        return sum;
+    while (i < nums.size() && nums[i] == nums[i - 1] + 1) {
+        sum += nums[i];
+        i++;
     }
-};
+
+    // Store all elements in a set
+    unordered_set<int> st(nums.begin(), nums.end());
+
+    // Find the smallest missing integer >= sum
+    while (st.count(sum)) {
+        sum++;
+    }
+
+    return sum;
+}
+
+int main() {
+
+    vector<int> nums = {3, 4, 5, 1, 2};
+
+    int ans = missingInteger(nums);
+
+    cout << "Answer: " << ans << endl;
+
+    return 0;
+}
