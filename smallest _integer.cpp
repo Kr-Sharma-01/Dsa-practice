80e490d40de0d2be794c1a87db496914b495c67c
Krish Sharma <krishsharma2425@gmail.com>
2026-08-12 23:54:52 +0530

    Leetcode

--- smallest _integer.cpp
+++ smallest _integer.cpp
@@ -12,30 +12,22 @@ int missingInteger(vector<int>& nums) {
 
     // Find sum of the sequential prefix
     int i = 1;
-
     while (i < nums.size() && nums[i] == nums[i - 1] + 1) {
         sum += nums[i];
         i++;
     }
-
     // Store all elements in a set
     unordered_set<int> st(nums.begin(), nums.end());
-
     // Find the smallest missing integer >= sum
     while (st.count(sum)) {
         sum++;
     }
-
     return sum;
 }
 
 int main() {
-
     vector<int> nums = {3, 4, 5, 1, 2};
-
     int ans = missingInteger(nums);
-
     cout << "Answer: " << ans << endl;
-
-    return 0;
+   return 0;
 }
