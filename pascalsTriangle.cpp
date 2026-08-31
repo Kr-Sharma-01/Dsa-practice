d922c1df45134d93004c7d2d4cfdc01f9d39e9b1
Krish Sharma <krishsharma2425@gmail.com>
2026-08-31 17:36:20 +0530

    Leetcode

--- pascalsTriangle.cpp
+++ pascalsTriangle.cpp
@@ -4,8 +4,20 @@
 
 using namespace std;
 
-// Brute Soln :
+// Optimal Soln :
 
-int triangle (vector<vector<int>> & arr){
-    
+vector<vector<int>> triangle (int Numrows){
+    vector<vector<int>> ans;
+
+    for(int i = 0 ; i < Numrows ; i++){
+
+        vector<int> row (i + 1 , 1);
+        
+        for(int j = 1 ; j < i ; j++){
+            row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+        }
+
+        ans.push_back(row);
+    }
+    return ans;
 }
