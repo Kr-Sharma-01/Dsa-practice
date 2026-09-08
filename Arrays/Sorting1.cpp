791e96173f61933e82e166b7b558a3bae0e3575e
Krish Sharma <krishsharma2425@gmail.com>
2026-09-08 23:54:58 +0530

    Topic

--- Arrays/Sorting1.cpp
+++ Arrays/Sorting1.cpp
@@ -1,5 +1,38 @@
+// Sorting (Psuedo codes)
+
 #include<bits/stdc++.h>
 
 using namespace std ;
 
-// Bubble Sort 
+// Selection Sort : TC = O(n^2)
+
+int selection (vector<int> & arr){
+    int n = arr.size();
+    for(int i = 0 ; i < n-2 ; i++){
+        int mini = i;
+        for(int j = i ; j < n-1 ; j++){
+            if (arr[j] < arr[mini]){
+            mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+
+// Bubble Sort : TC : O(n^2)
+
+int bubble (vector<int> & arr){
+    int n = arr.size();
+    for (int i = n-1 ; i <= 0 ; i--){
+        for(int j = 0 ; j <= i-1 ; j++){
+            if(arr[j] < arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
