921f2a6915cc5c91fea07a54008091114ee66931
Krish Sharma <krishsharma2425@gmail.com>
2026-09-10 23:58:11 +0530

    Topic

--- Arrays/Sorting1.cpp
+++ Arrays/Sorting1.cpp
@@ -21,7 +21,7 @@ void selection (vector<int> & arr){
     }
 }
 
-// Bubble Sort : TC : O(n^2)
+// Bubble Sort : TC : O(n^2) , best : O(n)
 
 void bubble (vector<int> & arr){
     int n = arr.size();
@@ -36,7 +36,7 @@ void bubble (vector<int> & arr){
     }
 }
 
-// Insertion Sort : TC : O()
+// Insertion Sort : TC : O(n^2) , best : O(n)
 
 void insertion (vector<int> & arr){
     int n = arr.size();
@@ -49,8 +49,61 @@ void insertion (vector<int> & arr){
     }
 }
 
-// Merge Sort : TC : O()
+// Merge Sort : TC : O(Log n * n)
 
-void merge (vector<int> & arr){
+void merge (vector<int> & arr , int low , int mid  , int high){
+    vector<int> temp ;
+    int left = low ;
+    int right = mid+1 ; 
+    while( left <= mid && right <= high){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+        }
+    }
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i = low ; i <= high ; i++){
+        arr[i] = temp[i-low];
+    }
+}
 
+void ms (vector<int> & arr , int low , int high){
+    if ( low == high) return;
+    int mid = (low + high) / 2;
+    ms(arr , low , mid);
+    ms(arr , mid+1 , high);
+    merge(arr , low , mid , high); 
+}
+
+// Quick Sort : TC : O(n Log n)
+
+int quick(int arr[], int low, int high) {
+    int pivot = arr[high];   
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void qs(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = quick(arr, low, high);
+        qs(arr, low, pi - 1);
+        qs(arr, pi + 1, high);
+    }
 }
