350c04f2cc3e93a3ae7366271a0047c046c2b235
Krish Sharma <krishsharma2425@gmail.com>
2026-09-15 23:53:44 +0530

    New Question

--- Arrays/countInversion.cpp
+++ Arrays/countInversion.cpp
@@ -2,3 +2,21 @@
 
 using namespace std ;
 
+// Brute Soln : TC : O(n^2)
+
+int count (vector<int> & arr){
+    int count = 0;
+    for (int i = 0 ; i < arr.size() ; i++){
+        for(int j = i+1; j < arr.size() ; j++){
+            if (arr[i] > arr[j]){
+                count += 1;
+            }
+        }
+    }    
+}
+
+// Optimal Soln : TC O(n)
+
+int CountInversions (vector<int> & arr){
+    
+}
