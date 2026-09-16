b5e799d8e8ee16f02c9f029292904c92f3007ac4
Krish Sharma <krishsharma2425@gmail.com>
2026-09-16 16:18:56 +0530

    New Question

--- Arrays/countInversion.cpp
+++ Arrays/countInversion.cpp
@@ -15,8 +15,44 @@ int count (vector<int> & arr){
     }    
 }
 
-// Optimal Soln : TC O(n)
+// Optimal Soln : TC O(n log n )
 
-int CountInversions (vector<int> & arr){
-    
+int CountInversion (vector<int> & arr , int low , int mid  , int high){
+    int count = 0;
+    vector<int> temp ;
+    int left = low ;
+    int right = mid+1 ; 
+    while( left <= mid && right <= high){
+        if(arr[left] <= arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            count += (mid - left + 1);
+            right++;
+        }
+    }
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i = low ; i <= high ; i++){
+        arr[i] = temp[i-low];
+    }
+    return count;
+}
+
+int ms (vector<int> & arr , int low , int high){
+    int cnt = 0;
+    if ( low == high) return;
+    int mid = (low + high) / 2;
+    cnt += ms(arr , low , mid);
+    cnt += ms(arr , mid+1 , high);
+    cnt += CountInversion(arr , low , mid , high); 
+    return cnt ;
 }
