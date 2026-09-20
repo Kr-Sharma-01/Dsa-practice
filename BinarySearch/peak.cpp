e5335581262bdd40f51987f72e99f1eef54a4487
Krish Sharma <krishsharma2425@gmail.com>
2026-09-20 22:58:22 +0530

    Leetcode

---
+++ BinarySearch/peak.cpp
@@ -0,0 +1,45 @@
+// Peak Element in Rotated Sorted Array (Leetcode)
+
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+// Brute Soln : TC O(N)
+
+int peak (vector<int> & arr){
+    int n = arr.size();
+
+    for(int i = 0 ; i < n ; i++){
+        if((i == 0 || arr[i-1] < arr[i]) && (i == n-1 || arr[i] > arr[i+1])){
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Optimal Soln : TC O(log n)
+
+int PeakElement (vector<int> & arr){
+    int n = arr.size();
+    int low = 1 , high = n-2;
+
+    if (n == 1) return 0;
+    if (arr[0] > arr[1]) return 0;
+    if(arr[n-1] > arr[n-2]) return n-1 ;
+
+    while(low <= high){
+        int mid = (low + high)/2;
+
+        if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){
+            return mid;
+        }
+
+        else if (arr[mid] > arr[mid-1]){
+            low = mid + 1 ;
+        }
+        else{
+            high = mid - 1 ;
+        }
+    }
+    return -1;
+}
