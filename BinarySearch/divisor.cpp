8709ee976bae3cd286016e44c5db8d387547d3ed
Krish Sharma <krishsharma2425@gmail.com>
2026-09-21 23:57:59 +0530

    Leetcode

---
+++ BinarySearch/divisor.cpp
@@ -0,0 +1,29 @@
+// Find the Smallest Divisor Given a Threshold (Leetcode)
+
+#include<bits/stdc++.h>
+
+using namespace std ;
+    int SumD(vector<int> & arr , int d){
+        int sum = 0 ;
+        int n = arr.size();
+
+        for(int i = 0 ; i < n ; i++){
+            sum = sum + ceil((double)(arr[i]) / (double)(d));
+        }
+        return sum;
+    }
+
+    int smallestDivisor(vector<int>& arr, int threshold) {
+        int low = 1 , high = *max_element(arr.begin() , arr.end());
+        while(low <= high){
+            int mid = (low + high)/2 ;
+
+            if(SumD(arr , mid) <= threshold){
+                high = mid -1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        return low ;
+    }
